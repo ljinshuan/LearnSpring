6b0327940018406eee00713b4796f0c24240fa28
ljinshuan <ljinshuan@gmail.com>
2015-03-07 14:09:40 +0800

    拦截器

---
+++ src/main/java/com/tmall/wireless/tel/interceptor/TestInterceptor.java
@@ -0,0 +1,35 @@
+package com.tmall.wireless.tel.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+
+
+public class TestInterceptor extends  HandlerInterceptorAdapter {
+	@Override
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler) throws Exception {
+		
+		if (request.getParameter("name").equals("ljinshuan")) {
+			return true;
+		}
+		return false;
+	}
+	@Override
+	public void postHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		
+		super.postHandle(request, response, handler, modelAndView);
+	}
+	@Override
+	public void afterCompletion(HttpServletRequest request,
+			HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		
+		super.afterCompletion(request, response, handler, ex);
+	}
+}
