34e62db3d4ae6290ddf199da6681db3a4afccbdb
ljinshuan <jinshuan@gmail.com>
2015-04-01 15:10:49 +0800

    depends-on

--- springbasic/src/main/java/com/tmall/wireless/springbasic/beans/WeatherService.java
+++ springbasic/src/main/java/com/tmall/wireless/springbasic/beans/WeatherService.java
@@ -1,6 +1,10 @@
 package com.tmall.wireless.springbasic.beans;
 
 public class WeatherService {
+	
+	public WeatherService() {
+		System.out.println("construct weather service");
+	}
 	public void getWeather(String data){
 		System.out.println(data+"WeatherService getWeather " );
 	}
