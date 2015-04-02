b1355af06b4d54336e355594f7ac3f2096326c18
ljinshuan <jinshuan@gmail.com>
2015-04-02 10:52:59 +0800

    通过aop config来配置 POJO

--- springbasic/src/main/java/com/tmall/wireless/spring/aop/SleepHelper.java
+++ springbasic/src/main/java/com/tmall/wireless/spring/aop/SleepHelper.java
@@ -1,21 +1,15 @@
 package com.tmall.wireless.spring.aop;
 
-import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class SleepHelper{
 	
-	@Pointcut("execution(* sleep(..))")
 	public void sleepPointcut(){}
 	
-	@Before("sleepPointcut()")
 	public void beforeSleep(){
 		System.out.println("beforeSleep");
 	}
-	@AfterReturning("sleepPointcut()")
 	public void afterReturn(){
 		System.out.println("afterReturn");
 	}
