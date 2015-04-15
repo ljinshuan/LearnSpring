92b99591c60d289ac655d87091353be227e957f2
ljinshuan <jinshuan@gmail.com>
2015-04-15 10:21:37 +0800

    返回json

--- rest/src/main/java/com/tmall/wireless/web/controller/HomeController.java
+++ rest/src/main/java/com/tmall/wireless/web/controller/HomeController.java
@@ -12,6 +12,6 @@ public class HomeController {
 	public String home(Model model){
 		model.addAttribute("username", "ljinshuan");
 		System.out.println("Home");
-		return "home";
+		return "index";
 	}
 }
---
+++ rest/src/main/java/com/tmall/wireless/web/controller/PersonController.java
@@ -0,0 +1,29 @@
+package com.tmall.wireless.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.tmall.wireless.web.model.Person;
+
+@RequestMapping(value="/person")
+public class PersonController {
+	
+	
+	@RequestMapping(value="/{id}",method=RequestMethod.GET)
+	@ResponseBody
+	public Person get(@PathVariable int id,HttpServletRequest request){
+	
+		Person person= new Person("ljinshuan", id);
+		return person;
+	}
+	
+	@RequestMapping(value="/test",method=RequestMethod.GET)
+	@ResponseBody
+	public String test(){
+		return "test";
+	}
+}
