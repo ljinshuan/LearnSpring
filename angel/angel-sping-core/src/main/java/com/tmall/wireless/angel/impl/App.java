1c36d6830cd28f3d5f11c3424a458931183d717d
ljinshuan <ljinshuan@gmail.com>
2016-03-13 01:56:32 +0800

    new beans

--- angel/angel-sping-core/src/main/java/com/tmall/wireless/angel/impl/App.java
+++ angel/angel-sping-core/src/main/java/com/tmall/wireless/angel/impl/App.java
@@ -1,19 +1,18 @@
 package com.tmall.wireless.angel.impl;
 
-import com.tmall.wireless.angel.MessageProvider;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.tmall.wireless.angel.MessageRender;
 
 public class App {
 
 	public static void main(String[] args) {
-		
-		
-		MessageRender messageRender=new StandardOutMessageRender();
-		
-		MessageProvider messageProvider=new HelloWorldMessageProvider();
-		
-		messageRender.setMessageProvider(messageProvider);
-		
+
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring/spring-beans.xml");
+		MessageRender messageRender = applicationContext.getBean("messageRender", MessageRender.class);
 		messageRender.render();
+		
+		
 	}
 }
