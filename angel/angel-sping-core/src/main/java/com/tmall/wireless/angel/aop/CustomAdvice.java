e0c8abf091e03ce6861ecff1e0389ec0b5cf769d
ljinshuan <ljinshuan@gmail.com>
2016-03-13 04:24:36 +0800

    aop

---
+++ angel/angel-sping-core/src/main/java/com/tmall/wireless/angel/aop/CustomAdvice.java
@@ -0,0 +1,28 @@
+package com.tmall.wireless.angel.aop;
+
+
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CustomAdvice {
+
+	private Logger logger=LoggerFactory.getLogger(CustomAdvice.class);
+	
+	public void beforeAdvice(JoinPoint joinpoint){
+		logger.info(joinpoint.toString());
+		logger.info("before");
+	}
+	
+	public Object aroundAdvice(ProceedingJoinPoint joinPoint) throws Throwable{
+		
+		logger.info(joinPoint.toLongString());
+		
+	
+		logger.info("after");
+		
+		return "1234";
+	}
+}
